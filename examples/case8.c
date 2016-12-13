631815eea5cfce80cf4764f0ac8b5693ed22ec08
Brandon Nguyen <nguyen.br@husky.neu.edu>
2016-12-13 12:13:50 -0500

    Another example case to demonstrate combining callbacks and function closures

---
+++ examples/case8.c
@@ -0,0 +1,24 @@
+// Case that demonstrates callback functions combined with closure functionality
+
+int foo(int b) {
+    int a;
+    a = 0;
+
+    int bar(int b) {
+        a = a + b;
+        return a;
+    }
+
+    return bar(b);
+}
+
+void baz(function f, int c) {
+    c = f(c);
+}
+
+int x;
+x = 1;
+
+baz(foo, x);            // a = 1, x = 1
+baz(foo, x);            // a = 2, x = 2
+baz(foo, x);            // a = 4, x = 4
